e5e95889442c00e8ad12cb84b8ab63602e447307
Jacksun <2231026419@qq.com>
2022-09-12 16:38:47 +0800

    第一次

---
+++ 计算.c/计算.c/jisuan.c
@@ -0,0 +1,13 @@
+int main ()
+{ 
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+		scanf("%d %d", &a, &b);
+			sum = a + b;
+		printf("sum = %d\n", sum);
+		system("pause");
+	//只能进行简单的加法计算	
+		return 0;
+}
+
